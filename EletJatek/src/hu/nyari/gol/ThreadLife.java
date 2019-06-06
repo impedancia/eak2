bf95188382a029d005dad359828c65a3af555b13
Nyári Norbert <nyari.norbert.87@gmail.com>
2019-06-06 17:42:13 +0200

    felszabdaltam range-ekre a feldolgozást, de egyelőre szekvenciálisan dolgozza fel az egyes range-eket, most jöhet a szálkezelés

--- EletJatek/src/hu/nyari/gol/ThreadLife.java
+++ EletJatek/src/hu/nyari/gol/ThreadLife.java
@@ -1,11 +1,79 @@
 package hu.nyari.gol;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** Game of Life. */
 public class ThreadLife extends Life {
 
 
+    private static int n;
+    private static int m;
+    private static int iterations;
+    private static int range_length;
+    private static int range_mod;
+    private List<Integer[]> ranges;
+
     public ThreadLife(int n, int m, boolean torus) {
         super(n, m, torus);
+    }
+
+    private static int degree_of_parallelism;
+
+    /** Compute new state for only one cell */
+    protected void one_step(int i, int j){
+               // System.out.println(("i: "+i+"j: "+j));
+                switch( living9(i,j) ){
+                    case  3: to[i][j] = true; break;
+                    case  4: to[i][j] = from[i][j]; break;
+                    default: to[i][j] = false;
+                }
+    }
+
+    protected void range_step(int f, int t){
+        for(int k = f; k<=t; k++)
+        {
+            int i = k / m;
+            int j = k % m;
+            one_step(i,j);
+        }
+    }
+    private void calculate_ranges(){
+        int number_of_cells = n*m;
+        range_length = number_of_cells / degree_of_parallelism;
+        range_mod = number_of_cells % degree_of_parallelism;
+
+        ranges = new ArrayList<Integer[]>();
+        for(int i=0; i<degree_of_parallelism;i++) {
+            Integer[] range =new Integer[2];
+            range[0] = i * range_length;
+            range[1] = (i+1) * range_length-1;
+            ranges.add(range);
+        }
+        if (range_mod > 0) {
+            Integer[] range = new Integer[2];
+            range[0] = number_of_cells - 1 - range_mod;
+            range[1] = number_of_cells - 1;
+            ranges.add(range);
+        }
+    }
+    public void iterate( int count ){
+        calculate_ranges();
+
+        while( count > 0 ){
+            int f =0;
+            int t =0;
+            for(int i=0; i<ranges.size();i++) {
+                Integer[] range = ranges.get(i);
+                f = range[0];
+                t = range[1];
+                System.out.println("f: "+f+"t: "+t);
+                range_step(f, t);
+            }
+
+            boolean[][] tmp = from; from = to; to = tmp;  // swap from and to
+            --count;
+        }
     }
 
     public static void main(String[] args ) throws Exception {
@@ -16,11 +84,11 @@ public class ThreadLife extends Life {
             new Life(500,500,false).gosperGliderGun(5,5).animate(200,1,100,0,0,50,80);
             new Life(300,300,false).acorn(150,150).animate(6000,100,0,140,100,20,70);
         } else {
-            int n = Integer.parseInt(args[0]);
-            int m = Integer.parseInt(args[1]);
-            int iterations = Integer.parseInt(args[2]);
-            int degree_of_parallelism = Integer.parseInt(args[5]);
-            Life life = new Life(n,m,false);
+            n = Integer.parseInt(args[0]);
+            m = Integer.parseInt(args[1]);
+            iterations = Integer.parseInt(args[2]);
+            degree_of_parallelism = Integer.parseInt(args[5]);
+            ThreadLife life = new ThreadLife(n, m,false);
             life.fromFile(args[3]);//.animate(iterations,1,100,0,0,n,m);
             System.out.println("Computation lasted "+ life.measure(iterations) +" milliseconds.");
             life.toFile(args[4]);
